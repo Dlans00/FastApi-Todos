fa3fab40d08117b9eab933ccc75afe686670412c
Dlans00 <lee.moonjeong@gmail.com>
2026-05-03 21:43:59 +0900

    [feat] main.py에 prometheus 추가

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse, FileResponse
 from pydantic import BaseModel
-
 from typing import List, Dict
 import json
 import os
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI()
+
+# Prometheus 메트릭스 엔드포인트 (/metrics)
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
 JSON_FILE = "todo.json"
 
